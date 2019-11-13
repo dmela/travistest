9e96a0119001534ff27e60afcec0b37a4763dc1b
Daniel Melander <daniel.melander@luke.fi>
2019-11-13 15:45:32 +0200

    More unit tests for addition

--- test_calc.py
+++ test_calc.py
@@ -4,6 +4,10 @@ import unittest
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
-        
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-4, -5), -9)
+        self.assertEqual(calc.add(-4, 5), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
