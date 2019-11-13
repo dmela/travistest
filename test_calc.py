d9f86c7e3f6c5eb297637e04f703e61233c8740e
Daniel Melander <daniel.melander@luke.fi>
2019-11-13 15:52:57 +0200

    Now each function has test cases

--- test_calc.py
+++ test_calc.py
@@ -8,6 +8,21 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.add(-4, -5), -9)
         self.assertEqual(calc.add(-4, 5), 1)
 
+    def test_substract(self):
+        self.assertEqual(calc.substract(100, 50), 50)
+        self.assertEqual(calc.substract(-40, -50), 10)
+        self.assertEqual(calc.substract(-40, 50), -90)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10, 5), 50)
+        self.assertEqual(calc.multiply(-4, -5), 20)
+        self.assertEqual(calc.multiply(-4, 5), -20)
+
+    def test_division(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-4, -2), 2)
+        self.assertEqual(calc.divide(-100, 50), -2)
+
 
 if __name__ == '__main__':
     unittest.main()
