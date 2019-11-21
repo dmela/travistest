c389dfac7a4cc3775f3884b591f42b858000ebaa
Daniel Melander <daniel.melander@luke.fi>
2019-11-21 11:13:59 +0200

    Unit test changes in division

--- test_ui.py
+++ test_ui.py
@@ -9,22 +9,22 @@ class TestUI(unittest.TestCase):
         ui.arr.append(1) 
         ui.arr.append(10) 
         ui.arr.append(20) 
-        self.assertEqual(ui.calculate(), 30) # 10 + 20 = 30
+        self.assertEqual(ui.calculate(), 30)
         ui.clearUserInput()
         ui.arr.append(2) 
         ui.arr.append(11) 
         ui.arr.append(20)
-        self.assertEqual(ui.calculate(), -9) # 10 - 20 = -9
+        self.assertEqual(ui.calculate(), -9)
         ui.clearUserInput()
         ui.arr.append(3) 
         ui.arr.append(10) 
         ui.arr.append(20)
-        self.assertEqual(ui.calculate(), 200) # 10 * 20 = 200
+        self.assertEqual(ui.calculate(), 200)
         ui.clearUserInput()
         ui.arr.append(4) 
-        ui.arr.append(10) 
-        ui.arr.append(20)
-        self.assertEqual(ui.calculate(), 0.5) # 10 / 20 = 0.5
+        ui.arr.append(20) 
+        ui.arr.append(10)
+        self.assertEqual(ui.calculate(), 2)
         ui.clearUserInput()
 
 if __name__ == '__main__':
